e64c54fa6a6c179882ee2706ac3e6383e837cc25
Daniël W. Crompton <daniel.crompton@gmail.com>
2015-10-11 11:30:24 +0200

    First arduino code, adapted form Blink and AnalogReadSerial

---
+++ AnalogReadSerialToBlink/AnalogReadSerialToBlink.ino
@@ -0,0 +1,27 @@
+/*
+  AnalogReadSerial
+  Reads an analog input on pin 0, prints the result to the serial monitor.
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+ This example code is in the public domain.
+ */
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+    pinMode(13, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0) + 1;
+  // print out the value you read:
+  Serial.println(sensorValue);
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(sensorValue);
+  digitalWrite(13, LOW);
+  delay(sensorValue);
+//  delay(1);        // delay in between reads for stability
+}
